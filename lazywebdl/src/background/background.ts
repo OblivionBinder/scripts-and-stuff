c3784066306e787363e54b9dad76025e20ede661
OblivionBinder <kimvt009@gmail.com>
2026-06-23 16:08:34 +0200

    Functioning redirection of sites (for the love of all that is holy, remember the manifest file...)

--- lazywebdl/src/background/background.ts
+++ lazywebdl/src/background/background.ts
@@ -1,71 +1,77 @@
-// import redirectedSites from './src/background/sites.ts';
-
 console.log("background.ts running, rr script ready0");
 
-var test = {
-  match: "*://*.royalroad.com/*",
-  regex: `(?:\S)*(:\/\/)(?:\S)*(.royalroad.com)\/`,
-  targetString: "www.royalroad.com",
-  replacementString: "www.dmi.dk"  
-}
+class SiteInfo {
+  siteURL: URL;
+  match: string;
+  targetString: string;
+  replacementString: string;
 
-var xitter = {
-  match: "*://*.x.com/*",
-  regex: `(?:\S)*(:\/\/)(?:\S)*(.royalroad.com)\/`,
-  targetString: "www.x.com",
-  replacementString: "www.xcancel.com"
+  constructor(url:string, match: string, targetString: string, replacementString: string) {
+    this.siteURL = new URL(url)
+    this.match = match;
+    this.targetString = targetString;
+    this.replacementString = replacementString;
+  };
 };
 
-var reddit = {
-  match: "*://*.reddit.com/*",
-  regex: `(?:\S)*(:\/\/)(?:\S)*(.royalroad.com)\/`,
-  targetString: "www.reddit.com",
-  replacementString: "old.reddit.com"
-};
+var test = new SiteInfo(
+  "https://www.example.com/",
+  "*://*.example.com/*", 
+  "example.com", 
+  "www.example.org"
+);
 
-var redirectedSites: Map<string, any> = new Map();
-redirectedSites.set("test", test)
-redirectedSites.set("xitter", xitter)
-redirectedSites.set("reddit", reddit)
+// var xitter = new SiteInfo(
+//   "*://*.x.com/*", 
+//   "www.x.com", 
+//   "www.xcancel.com");
 
+// var reddit = new SiteInfo(
+//   "*://*.reddit.com/*", 
+//   "www.reddit.com", 
+//   "old.reddit.com");
 
+var redirectedSites: SiteInfo[] = [test];
+// var redirectedSites: SiteInfo[] = [test, xitter, reddit];
 
 function redirectTest(requestDetails: any) {
-  let redirect = "URL NOT FOUND!?!";
+  let requestURL = new URL(requestDetails.url);
 
-  for (const value of redirectedSites.values()) {
-    let webSearch = new RegExp(`${value.regex}`);
-    if (requestDetails.url.search(webSearch)) {
-      redirect = requestDetails.url.replace("www.reddit.com", "old.reddit.com");
-      console.log(`redirecting from ${requestDetails.url} to ${redirect}`);
+  for (const siteInfo of redirectedSites) {
+    
+    if (requestURL.hostname.endsWith("." + siteInfo.targetString)) {
+      requestURL.hostname = siteInfo.replacementString;
+      // const redirect = requestURL.hostname siteInfo.replacementString)
 
+      console.log(`redirecting from ${requestDetails.url} to ${requestURL.toString()}`);
+      return { redirectUrl: requestURL.toString() };
     }
-
   }
 
 
+  // for (const siteInfo of redirectedSites) {
+  //   const regex = new RegExp(`^https?://([^/]*\\.)?${siteInfo.targetString}(/|$)`);
+    
+  //   if (regex.test(requestDetails.url)) {
+  //     let redirect = requestDetails.url.replace(siteInfo.targetString, siteInfo.replacementString);
+      
+  //     console.log(`redirecting from ${requestDetails.url} to ${redirect}`);
+      
+  //     return { redirectUrl: redirect };  
+  //   }
+  // }
 
-  // const redirect = requestDetails.url.replace("royalroad.com", "dmi.dk");
-
-  return {
-    redirectUrl:
-      redirect,
-  };  
+  return {};
 };
 
 
 browser.webRequest.onBeforeRequest.addListener(
   redirectTest, 
-  { urls: [test.match, xitter.match, reddit.match] }, 
+  { urls: [test.match] }, 
+  // { urls: [test.match, xitter.match, reddit.match] }, 
   ["blocking"],
 );
 console.log("Listener registered:", browser.webRequest.onBeforeRequest.hasListener(redirectTest));
 
-// browser.commands.onCommand.addListener((command) => {
-//   if (command === "run-command") {
-//     console.log("Custom command triggered!");
-//     // Add your logic here
-//   }
-// });
 
 
